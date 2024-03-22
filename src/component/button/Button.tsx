0f94f9d87f9dd2ddb57a344fa1f68fdf128063fb
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-03-22 09:45:31 +0300

    refact component type

--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import './button.css';
 import {ButtonPropsType} from "../../types/types";
 
-export const Button = (props: ButtonPropsType) => {
-    const {name, callback, disabled, error, block} = props;
+export const Button = ({name, callback, disabled, error, block}: ButtonPropsType) => {
 
     return (
         <button onClick={callback} className={'button'} disabled={error || disabled || block}>
